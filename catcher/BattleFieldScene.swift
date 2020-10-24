33e893082a43d5597ef480d5bca383e66a268b51
az@dostavista.ru <>
2020-10-24 15:46:56 +0300

    remove borders

--- catcher/BattleFieldScene.swift
+++ catcher/BattleFieldScene.swift
@@ -19,20 +19,16 @@ class BattleFieldScene: SKScene, SKPhysicsContactDelegate {
     private let hero = Hero(radius: 5)
     
     private var fallSpeed = CGFloat(-4)
-   
-    private var leftBorder:SKShapeNode!
-    private var rightBorder:SKShapeNode!
     
     var battleDelegate: BattleDelegate?
 
     let heroMask = Mask(category: 0b0011, collision: 0b0010, contact: 0b0011)
-    let borderMask = Mask(mask: 0b0010)
     let obstacleMask = Mask(category: 0b0001, collision: 0b0000, contact: 0b0001)
     
     var obstacleArranger: ObstacleArranger!
     
     func startBattle() {
-        obstacleArranger = ObstacleArranger(scene: self, startPointY: hero.position.y - 400, leftBorderX: leftBorder.frame.maxX, rightBorderX: rightBorder.frame.minX, obstacleMask: obstacleMask)
+        obstacleArranger = ObstacleArranger(scene: self, startPointY: hero.position.y - 400, leftBorderX: frame.minX, rightBorderX: frame.maxX, obstacleMask: obstacleMask)
         obstacleArranger.arrange()
     }
     
@@ -51,32 +47,6 @@ class BattleFieldScene: SKScene, SKPhysicsContactDelegate {
         
         physicsWorld.contactDelegate = self
         
-        leftBorder = SKShapeNode(rect: CGRect(x: 0, y: frame.minY, width: 1, height: frame.height))
-        let leftBorderBody = SKPhysicsBody(edgeLoopFrom: leftBorder.frame)
-        leftBorderBody.affectedByGravity = false
-        leftBorderBody.pinned = true
-        leftBorderBody.restitution = 0
-        leftBorderBody.friction = 0
-        leftBorderBody.set(mask: borderMask)
-        leftBorder.physicsBody = leftBorderBody
-        leftBorder.fillColor = .clear
-        leftBorder.strokeColor = .clear
-        
-        rightBorder = SKShapeNode(rect: CGRect(x: frame.maxX, y: frame.minY, width: 1, height: frame.height))
-        let rightBorderBody = SKPhysicsBody(edgeLoopFrom: rightBorder.frame)
-        rightBorderBody.affectedByGravity = false
-        rightBorderBody.pinned = true
-        rightBorderBody.restitution = 0
-        rightBorderBody.friction = 0
-        rightBorderBody.set(mask: borderMask)
-        rightBorder.physicsBody = rightBorderBody
-        rightBorder.fillColor = .clear
-        rightBorder.strokeColor = .clear
-        
-        addChild(leftBorder)
-        addChild(rightBorder)
-        
-        
     }
     
     override func update(_ currentTime: TimeInterval) {
@@ -87,8 +57,6 @@ class BattleFieldScene: SKScene, SKPhysicsContactDelegate {
     func adjustCameraAndBorders() {
         let newPositionY = hero.position.y - frame.height/2 + 50
         camera?.position.y = newPositionY
-        leftBorder.position.y = newPositionY
-        rightBorder.position.y = newPositionY
     }
     
     func didBegin(_ contact: SKPhysicsContact) {
