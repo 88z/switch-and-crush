9f546ae4a2ff5889073ef4452e43f5f1cd147117
sasha zhuravlev <paperairplane89@gmail.com>
2022-06-29 00:17:42 +0300

    file was added

---
+++ catcher/Presenters/BasePresenter.swift
@@ -0,0 +1,51 @@
+//
+//  BasePresenter.swift
+//  catcher
+//
+//  Created by Aleksandr Zhuravlev on 25.10.2020.
+//
+
+
+
+import Foundation
+import SpriteKit
+
+class BasePresenter: NSObject, GamePresenter, SKSceneDelegate, BattleDelegate, OneActionSceneDelegate {
+    
+    private var backgroundManager: InfiniteBackgroundManager?
+    public weak var vc: GameViewController?
+    public weak var battleFieldScene: BattleFieldScene!
+    
+    public let heroTopOffset = CGFloat(250)
+    required init (vc: GameViewController) {
+        self.vc = vc
+    }
+
+    func present(){
+        let battleFieldScene = BattleFieldScene(size: UIScreen.main.bounds.size, heroTopOffset: heroTopOffset)
+        battleFieldScene.battleDelegate = self
+        battleFieldScene.scaleMode = .resizeFill
+        battleFieldScene.delegate = self
+        backgroundManager = InfiniteBackgroundManager(scene: battleFieldScene, textureGenerator: GridGenerator())
+        vc?.showBattleField(scene: battleFieldScene)
+        self.battleFieldScene = battleFieldScene
+    }
+    
+    func update(_ currentTime: TimeInterval, for scene: SKScene) {
+        scene.update(currentTime)
+        backgroundManager?.swapIfNeeded()
+    }
+    
+    
+    func crashed() {
+    
+    }
+    
+    func didFinish(level:Level) {
+        
+    }
+    
+    func oneActionScenePressed(scene: OneActionScene) {
+        
+    }
+}
