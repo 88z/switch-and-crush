5f488a762bcdbc1a4f3343e95dd8afd935e8296f
sasha zhuravlev <paperairplane89@gmail.com>
2023-01-27 21:11:42 +0300

    level failed events

--- catcher/Presenters/LevelFailedPresenter.swift
+++ catcher/Presenters/LevelFailedPresenter.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import SpriteKit
+import Amplitude
 
 class LevelFailedPresenter:Presenter {
     public weak var vc: GameViewController?
@@ -15,6 +16,8 @@ class LevelFailedPresenter:Presenter {
     private let level: Level
     private let progress: Progress
     private let gameMode: GameMode
+    private let index: Int?
+    private let score:Int
     
     init(vc: GameViewController,
          level:Level,
@@ -25,12 +28,15 @@ class LevelFailedPresenter:Presenter {
         self.level = level
         self.progress = progress
         self.gameMode = gameMode
+        self.score = score
         
         let indexStr: String
         if let index = progress.index(of: level) {
             indexStr = String(index+1)
+            self.index = index
         } else {
             indexStr = ""
+            self.index = nil
         }
         
         if gameMode == .arcade {
@@ -42,9 +48,19 @@ class LevelFailedPresenter:Presenter {
     }
     
     func present() {
+        Amplitude.instance().logEvent("LevelFailed_Opened",
+                                      withEventProperties: ["level_index": self.index ?? "",
+                                                            "level_name":  self.level.name,
+                                                            "is_endless":  self.level.isEndless,
+                                                            "score": self.score])
         let gameOverView = TitleButtonsView(frame: .zero,
                                             buttonModels: [
             ButtonViewModel(text: "try again".localiz(), action: {
+                Amplitude.instance().logEvent("LevelFailed_TryAgain_Taped",
+                                              withEventProperties: ["level_index": self.index ?? "",
+                                                                    "level_name":  self.level.name,
+                                                                    "is_endless":  self.level.isEndless,
+                                                                    "score": self.score])
                 guard let vc = self.vc else {
                     assertionFailure("viewController no found")
                     return 
@@ -53,6 +69,11 @@ class LevelFailedPresenter:Presenter {
                 GamePresenter(vc: vc, startState: .first, level: self.level, progress: self.progress, gameMode: self.gameMode).present()
             }),
         ], title: title, topText: topText, imageName: "deadFace", backButtonIcon: .home, backButtonAction: {
+            Amplitude.instance().logEvent("LevelFailed_Back_Taped",
+                                          withEventProperties: ["level_index": self.index ?? "",
+                                                                "level_name": self.level.name,
+                                                                "is_endless": self.level.isEndless,
+                                                                "score": self.score])
             guard let vc = self.vc else {
                 assertionFailure("viewController no found")
                 return
