f8ed38a0e2b69ab0d73e9cc19ee87423b74fbafb
sasha zhuravlev <paperairplane89@gmail.com>
2023-02-11 00:42:42 +0300

    4.5 secs )

--- catcher/BattleFieldScene.swift
+++ catcher/BattleFieldScene.swift
@@ -103,7 +103,7 @@ class BattleFieldScene: SKScene, SKPhysicsContactDelegate {
     func startImmortal() {
         hero?.state = .immortal
         hero?.run(SKAction.applyImpulse(CGVector(dx: 0, dy: -8), duration: 2))
-        DispatchQueue.main.asyncAfter(deadline: .now() + 3) { [weak self] in
+        DispatchQueue.main.asyncAfter(deadline: .now() + 4.5) { [weak self] in
             self?.removeHero()
             self?.lastObstacle = self?.obstacleArranger?.arrangeNext(speed: self?.level?.initialSpeed ?? 0)
         }
